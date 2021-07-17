16a7eff59b271552738d8e379953cd2aa97ae5dd
Ojhal Patwa <patkarojhal26@gmail.com>
2021-07-17 23:32:03 +0530

    add new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import LifeCycleA from './LifeCycleA';
+import Product from './product';
 import './App.css';
 
 function App() {
   return (
-    <LifeCycleA></LifeCycleA>
+    <div>
+      <LifeCycleA></LifeCycleA>
+      <Product></Product>
+    </div>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class Product extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+export default Product;
+
+
+class Cart extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      qty: 0;
+    }
+  }
+
+}
+export default Cart;
